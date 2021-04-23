25ece3ff354329c61032e27161cae1b88700a896
Michael Shimniok <shimniok@gmail.com>
2021-04-23 11:07:40 -0600

    cleanup unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component, VERSION, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 import { ScrollToBottomDirective } from "./scroll-to-bottom.directive";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   @ViewChild(ScrollToBottomDirective)
   scroll: ScrollToBottomDirective;
 
-  showSettings = false;
-
   constructor() {}
 
-  toggleSettings() {
-    this.showSettings = !this.showSettings;
-  }
+  ngOnInit() {}
 }
