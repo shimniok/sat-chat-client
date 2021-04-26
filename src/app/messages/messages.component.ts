1ff31f3932f4894757afb0925545a7559f313538
Michael Shimniok <shimniok@gmail.com>
2021-04-26 10:10:17 -0600

    fixed scroll to bottom errors

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-import { interval } from "rxjs/internal/observable/interval";
-import { startWith, switchMap } from "rxjs/operators";
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 import { MessageService } from "../message.service";
 import { Message } from "../message-type";
@@ -11,10 +9,16 @@ import { Observable } from "rxjs";
   templateUrl: "./messages.component.html",
   styleUrls: ["./messages.component.css"],
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, AfterViewChecked {
+  @ViewChild("scrollBottom") private bottom: ElementRef;
+
   messages: Observable<Message[]> = this.messageService.getMessages();
 
   constructor(private messageService: MessageService) {}
+
+  ngAfterViewChecked() {
+    this.bottom.nativeElement.scrollIntoView();
+  }
 
   ngOnInit() {
   }
--- src/app/scroll-to-bottom.directive.ts
+++ src/app/scroll-to-bottom.directive.ts
@@ -1,13 +1,41 @@
-import { Directive, ElementRef } from "@angular/core";
+import {
+  AfterViewChecked,
+  Directive,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 
 @Directive({
-  selector: "[scroll-to-bottom]"
+  selector: "[scroll-to-bottom]",
 })
-export class ScrollToBottomDirective {
+export class ScrollToBottomDirective implements OnInit, AfterViewChecked {
+  // @ViewChild("scrollMe") private myScrollContainer: ElementRef;
+  @ViewChild("scrollBottom") private bottom: ElementRef;
+
   constructor(private _el: ElementRef) {}
 
+  ngAfterViewChecked(): void {
+    console.log("ScrollToBottom: ngAfterViewChecked()");
+    this.scrollToBottom();
+  }
+
+  ngOnInit(): void {
+    console.log("ScrollToBottom: ngOnInit");
+    this.scrollToBottom();
+  }
+
   public scrollToBottom() {
-    const el: HTMLDivElement = this._el.nativeElement;
-    el.scrollTop = Math.max(0, el.scrollHeight - el.offsetHeight);
+    console.log("ScrollToBottom: scrollToBottom()");
+    this.bottom.nativeElement.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "start",
+    });
+    // try {
+    //   this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    // } catch (err) {}
+    // const el: HTMLDivElement = this._el.nativeElement;
+    // el.scrollTop = Math.max(0, el.scrollHeight - el.offsetHeight);
   }
 }
