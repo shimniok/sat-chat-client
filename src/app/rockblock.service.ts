ab307623a35c4c308f20db4baadb089704de9e5e
Michael Shimniok <shimniok@gmail.com>
2021-04-30 12:08:13 -0600

    refactor rockblock service

--- src/app/rockblock.service.ts
+++ src/app/rockblock.service.ts
@@ -5,12 +5,12 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class RockblockService {
-  headers = { "content-type": "application/json" };
+  options = { headers: { "content-type": "application/json" } };
 
   send(messageText: string) {
     let url = "/api/send";
     let body = { message: messageText };
-    return this.http.post(url, body, { headers: this.headers });
+    return this.http.post(url, body, this.options);
   }
 
   constructor(private http: HttpClient) {}
