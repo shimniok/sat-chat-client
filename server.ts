5ab1a278f2ee978b8e0e594e088592aed525d812
Michael Shimniok <shimniok@gmail.com>
2021-05-16 17:17:47 -0600

    tweak https forwarding behavior

--- server.ts
+++ server.ts
@@ -11,30 +11,30 @@ console.log("proxying requests to " + apiUrl);
 
 app.use(express.static(__dirname + "/angular-build"));
 
-app.enable("trust proxy"); // trust proxy header
+//app.enable("trust proxy"); // trust proxy header
 app.use((req, res, next) => {
-  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+  if (req.headers["x-forwarded-proto"] === "http") {
+    res.redirect("https://" + req.headers.host + req.url);
+  } else {
+    next();
+  }
 });
 
 app.all("/api/*", function (req, res) {
   console.log(req.headers.host + " " + req.method + " " + req.url);
-  console.log(apiUrl+req.url);
+  console.log(apiUrl + req.url);
   try {
     apiProxy.web(req, res, { target: apiUrl, changeOrigin: true });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 });
 
 app.get("/*", function (req, res) {
-  if (req.secure) {
-    res.sendFile(path.join(__dirname, "angular-build", "index.html"));
-  } else {
-    res.redirect("https://" + req.headers.host + req.url);
-  }
+  res.sendFile(path.join(__dirname, "angular-build", "index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-var listener = app.listen(process.env.PORT || 4200, function() {
+var listener = app.listen(process.env.PORT || 4200, function () {
   console.log("Listening on port " + listener.address().port);
 });
